f1289bbedbadc492123947f6d5001ea48cc959c7
tourze <tourze@users.noreply.github.com>
2025-06-03 20:45:16 +0800

    feat(wechat-work-bundle): 添加 ResolveTargetEntityPass 处理 CorpInterface

--- src/WechatWorkBundle.php
+++ src/WechatWorkBundle.php
@@ -2,8 +2,23 @@
 
 namespace WechatWorkBundle;
 
+use Symfony\Component\DependencyInjection\Compiler\PassConfig;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use Tourze\DoctrineResolveTargetEntityBundle\DependencyInjection\ResolveTargetEntityPass;
+use Tourze\WechatWorkContracts\CorpInterface;
+use WechatWorkBundle\Entity\Corp;
 
 class WechatWorkBundle extends Bundle
 {
+    public function build(ContainerBuilder $container): void
+    {
+        parent::build($container);
+
+        $container->addCompilerPass(
+            new ResolveTargetEntityPass(CorpInterface::class, Corp::class),
+            PassConfig::TYPE_BEFORE_OPTIMIZATION,
+            1000,
+        );
+    }
 }
